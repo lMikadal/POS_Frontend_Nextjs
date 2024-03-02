0c5235fbacef8f054d6d2a0bc7f647069abae075
Mikada <panupong.mikada@gmail.com>
2024-03-02 21:35:52 +0700

    edit responsive main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
     <>
-      <div className="grid md:grid-cols-3 max-h-screen">
-        <div className="md:col-span-2">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 max-h-screen">
+        <div className="lg:col-span-2">
           <div className="px-4">
             <h1>เนื้อหา</h1>
           </div>
         </div>
-        <div className="bg-gray-100 hidden md:block overscroll-contain">
+        <div className="bg-gray-100 hidden sm:block overscroll-contain">
           <div className="px-4">
             <h1>หน้าแรก</h1>
             <h1>หน้าแรก</h1>
